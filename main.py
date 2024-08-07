43bc39661a4177441c9f74340ab8e3a8ec6279e0
EmixamL <emixamlaferriere@gmail.com>
2024-08-06 22:37:19 -0400

    Added style sheets

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_url_path="", static_folder="static")
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("layout.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
